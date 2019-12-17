50da31ff44aba08bb06c66b900a030220e688c05
Harvey Rogers <harveyr@gmail.com>
2019-12-16 16:46:44 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import * as core from '@actions/core'
 import * as kit from '@harveyr/github-actions-kit'
 import * as github from '@actions/github'
 
-const BRANCH_PREFIX = 'refs/heads/versions/'
+const REF_PREFIX = 'refs/heads/versions/'
 
 async function run(): Promise<void> {
+  const githubToken = core.getInput('github_token')
+
   const context = github.context
   // const { ref } = context.ref
   console.log('context', JSON.stringify(context, null, 2))
@@ -13,21 +15,18 @@ async function run(): Promise<void> {
     throw new Error('No repository found in Github payload. Cannot continue.')
   }
 
+  const { ref, actor } = context
+
   const repoName = context.payload.repository.name
   const repoOwner = context.payload.repository.owner.login
   console.log('Parsed repo: %s/%s', repoName, repoOwner)
 
-  const githubUsername = core.getInput('github_username')
-  const githubToken = core.getInput('github_token')
-
-  const branch = core.getInput('branch')
-  if (branch.indexOf(BRANCH_PREFIX) !== 0) {
-    throw new Error(
-      `Branch must begin with "${BRANCH_PREFIX}". Got "${branch}".`,
-    )
+  if (ref.indexOf(REF_PREFIX) !== 0) {
+    throw new Error(`Ref must begin with "${REF_PREFIX}". Got "${ref}".`)
   }
+  const releaseBranch = ref.replace(REF_PREFIX, 'releases/')
 
-  // const releaseBranch = branch.replace(BRANCH_PREFIX, 'releases/')
+  const commitPrefix = `Auto commit on behalf of ${actor}`
 
   // await kit.execAndCapture('git', ['checkout', '-b', releaseBranch])
   await kit.execAndCapture('git', [
@@ -47,7 +46,7 @@ async function run(): Promise<void> {
   await kit.execAndCapture('git', ['add', '-f', 'lib'])
   let changedOutput = await kit.execAndCapture('git', ['status', '-s'])
   if ((changedOutput.stderr + changedOutput.stdout).length) {
-    await kit.execAndCapture('git', ['commit', '-m', 'Auto commit: build'])
+    await kit.execAndCapture('git', ['commit', '-m', `${commitPrefix}: build`])
   }
 
   await kit.execAndCapture('npm', ['ci', '--only=production'])
@@ -57,19 +56,16 @@ async function run(): Promise<void> {
     await kit.execAndCapture('git', [
       'commit',
       '-m',
-      'Auto commit: node_modules',
+      `${commitPrefix}: node_modules`,
     ])
   }
 
-  if (!githubUsername) {
-    console.log('No Github username provided. Not pushing.')
-  }
   if (!githubToken) {
     console.log('No Github token provided. Not pushing.')
   }
 
-  // const remote = `https://${githubUsername}:${githubToken}@github.com/${REPOSITORY}.git`
-  // await kit.execAndCapture('git', ['push', remote, `HEAD:${releaseBranch}`])
+  const remote = `https://${actor}:${githubToken}@github.com/${repoOwner}/${repoName}.git`
+  await kit.execAndCapture('git', ['push', remote, `HEAD:${releaseBranch}`])
 }
 
 run().catch(err => {
