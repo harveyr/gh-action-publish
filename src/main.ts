6e983e6a1ecc80b0afcc507442cd548fee511903
Harvey Rogers <harveyr@gmail.com>
2019-12-16 16:10:51 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import * as core from '@actions/core'
 
+const BRANCH_PREFIX = 'refs/heads/versions/'
+
 async function run(): Promise<void> {
-  console.log('Hello cyber')
+  const branch = core.getInput('branch')
+  if (branch.indexOf(BRANCH_PREFIX) !== 0) {
+    throw new Error(`Branch must begin with "${BRANCH_PREFIX}". Got "${branch}".`)
+  }
+
+  const releaseBranch = branch.replace(BRANCH_PREFIX, 'releases/')
+
+  console.log(`Release branch will be: ${releaseBranch}`)
 }
 
 run().catch(err => {
